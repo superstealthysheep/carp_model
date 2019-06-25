c03014f7ba1f4797ff9b4c76153229d69ed7e21d
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2019-06-25 14:38:40 -0500

    Complete version 1 of carp_model_2.py
    It is now functional! It models the carp populations in each lake using a logistic curve, and also models fish travelling from lake to lake.

--- carp_model_2.py
+++ carp_model_2.py
@@ -1,4 +1,9 @@
+"""
+Measures carp spread from lake to lake over time
+"""
+#import mathplotlib.pyplot as plt
 import networkx as nx
+
 reproductive_constant = .01
 r = 2
 
@@ -10,6 +15,7 @@ class Lake:
     self.value = value
     self.carrying_capacity = carrying_capacity
     self.population = population
+    self.change_in_population = 0 #this is used for storing values from calculate_fish_spread
 
   def reproduce(self):
     m = (r-1)/self.carrying_capacity
@@ -17,15 +23,26 @@ class Lake:
     self.population = (r - m * self.population) * self.population
 
   def __repr__(self):
-    return {"value" : self.value,
+    return str({"value" : self.value,
             "carrying_capacity" : self.carrying_capacity,
-            "population" : self.population}
+            "population" : self.population})
   
   def __str__(self):
     return "Value: {}, Carrying Cap: {}, Population: {}".format(self.value, self.carrying_capacity, self.population)
 
-  def spread():
-    
+  def calculate_fish_spread(self): #caluclates how much the lake's population will change when fish randomly spread but does NOT actually change the population.
+    change_in_population = 0
+    for neighbor in list(lake_graph.neighbors(self)): #for loop iterating through all of the lakes neighboring self
+      #below is how many fish self will gain from its neighbors
+      self.change_in_population += (neighbor.population * lake_graph.edges[neighbor, self]["passing_probability"])
+      #lake_graph.edges[neighbor, self]["passing_probability"] is the passing probability of the connection between self and neighbor, i.e. the probability for a particular fish in either of the lakes to travel to the other lake
+
+      #below is how many fish self will lose to its neighbors
+      self.change_in_population += (-1 * self.population * lake_graph.edges[neighbor, self]["passing_probability"])
+
+  def commit_fish_spread(self): #puts the population effects from calculate_fish_spread() into effect. The reason why the two parts are split apart is that, if they weren't, the first lakes' populations would change before the other lakes could calculate how their populations should change. This would throw the caluclations off and fish wouldn't be conserved anymore.
+    self.population += self.change_in_population
+    self.change_in_population = 0
 
 lake_graph = nx.Graph()
 
@@ -48,5 +65,9 @@ for season in range(0, 20):
 
   for lake in lake_graph.nodes():
     lake.reproduce()
+    lake.calculate_fish_spread()
+
+  for lake in lake_graph.nodes():
+    lake.commit_fish_spread()
 
 
