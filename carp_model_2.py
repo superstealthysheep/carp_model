2152dadceb0ae304a014664afdeafe3f986a1c68
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2019-06-26 22:50:45 -0500

    Clean up after iterative logistic model
    Deleted now-obsolete and commented-out code having to do with the old iterative model of logistic growth.

--- carp_model_2.py
+++ carp_model_2.py
@@ -5,33 +5,26 @@ Measures carp spread from lake to lake over time
 import networkx as nx
 import numpy as np
 
-reproductive_constant = 1 
-reproductive_resolution = 1 ##should only be used for iterative model of logistic growth
-r = 1 + reproductive_constant/reproductive_resolution #r should not be less than 1. Even if r=1, the populations do not grow at all.
+r = 1
 
 number_of_spreads_per_season = 1
-
-#((2*m*c)^2)
-
 
 class Lake:
   def __init__(self, value, carrying_capacity, population):
     self.value = value
     self.carrying_capacity = carrying_capacity
     self.population = population
-    self.change_in_population = 0 #this is used for storing values from calculate_fish_spread
+    self.change_in_population = 0 #this is used for storing values from calculate_fish_spread()
 
   def reproduce(self):
-    """#uses an iterative model of logistic growth
-    m = (r-1)/self.carrying_capacity
+    #uses a continuous model of logistic growth based on differential equations
 
-    self.population = (r - m * self.population) * self.population"""
+    if self.population == self.carrying_capacity:
+      return None #if the population is at the carrying capacity, escape the function because otherwise it will divide by zero
 
-    #uses a continuous model based on differential equations
-    r = reproductive_constant
     c = self.population / (self.carrying_capacity - self.population)
     t = 1
-    self.population = self.carrying_capacity * c * np.exp(r * t) / (1 + c * np.exp(r * t))
+    self.population = self.carrying_capacity * c * np.exp(r * t) / (1 + c * np.exp(r * t)) #as long as self.population and t are positive, c * np.exp(r * t)) should be nonzero, so dividing by zero should not be an issue.
  
   def __repr__(self):
     return str({"value" : self.value,
@@ -59,7 +52,7 @@ lake_graph = nx.Graph()
 
 lake_dict = {"l1" : Lake(100, 1000, 5), 
              "l2" : Lake(50, 10, 0)}
-connection_list = [(lake_dict["l1"], lake_dict["l2"], {"passing_probability": .1})]
+connection_list = [(lake_dict["l1"], lake_dict["l2"], {"passing_probability": .01})]
 
 lake_graph.add_nodes_from(lake_dict.values()) #the .values is so that the Lake objects, not the keys, are added to the lake_graph.
 lake_graph.add_edges_from(connection_list)
@@ -71,14 +64,11 @@ for season in range(0, 100):
   print("Lakes:")
   for lake in lake_graph.nodes():
     print(str(lake))
-
   print("")
 
   #fish reproduction
   for lake in lake_graph.nodes():
-    for i in range(0,reproductive_resolution):
-      lake.reproduce()
-
+    lake.reproduce()
   #fish spreading
   for i in range(0, number_of_spreads_per_season):  
     for lake in lake_graph.nodes():
